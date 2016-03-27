e295318cc5e9bbca1eafa0e389d91802654bd5ae
Tautvydas <tautvisv@gmail.com>
2016-03-27 23:05:25 +0300

    user settings
    added user settings service

--- HolidaysPlanningTool/src/HolidaysPlanningToolWeb/wwwroot/userPanel/user-controls.js
+++ HolidaysPlanningTool/src/HolidaysPlanningToolWeb/wwwroot/userPanel/user-controls.js
@@ -1,7 +1,7 @@
 "use strict";
 var browser_1 = require('angular2/platform/browser');
 var user_panel_component_1 = require('./user-panel.component');
-var notification_service_1 = require('../app/utils/notification.service');
+//import {NotificationService} from '../app/utils/notification.service';
 var mock_data_service_1 = require('./../app/utils/mock-data.service');
-browser_1.bootstrap(user_panel_component_1.UserPanel, [notification_service_1.NotificationService, mock_data_service_1.MyNumber]);
+browser_1.bootstrap(user_panel_component_1.UserPanel, [mock_data_service_1.MyNumber]);
 //# sourceMappingURL=user-controls.js.map
--- HolidaysPlanningTool/src/HolidaysPlanningToolWeb/wwwroot/userPanel/user-controls.ts
+++ HolidaysPlanningTool/src/HolidaysPlanningToolWeb/wwwroot/userPanel/user-controls.ts
@@ -1,5 +1,6 @@
 ﻿import {bootstrap}  from 'angular2/platform/browser';
 import {UserPanel} from './user-panel.component';
+//import {NotificationService} from '../app/utils/notification.service';
 import {MyNumber} from './../app/utils/mock-data.service';
 
-bootstrap(UserPanel, [NotificationService, MyNumber]);
+bootstrap(UserPanel, [ MyNumber]);
